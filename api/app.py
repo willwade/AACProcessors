27a95f6355cedb3e4863f05ce67754582a665045
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2025-01-24 21:44:29 +0000

    Import gridset processor

--- api/app.py
+++ api/app.py
@@ -1,7 +1,17 @@
+import sys
+import os
+
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi import FastAPI, File, UploadFile
+from aac_processors import GridsetProcessor  # Add this import
 
 app = FastAPI()
 
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile):
+    processor = GridsetProcessor()
+    
+
     return {"filename": file.filename}
