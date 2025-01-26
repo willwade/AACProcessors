487efe39ac64515b4fbd8bb2b2d12d78d95f5614
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2025-01-26 20:59:38 +0000

    Process gridfile

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 --worker-tmp-dir /dev/shm api.app:app
+
+# gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 api.app:app
--- api/app.py
+++ api/app.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import shutil
+from tempfile import NamedTemporaryFile
+from pathlib import Path
+from fastapi.responses import FileResponse
 
 # Add the parent directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -9,31 +13,28 @@ from aac_processors import GridsetProcessor
 
 app = FastAPI()
 
+def save_upload_file_tmp(upload_file: UploadFile) -> Path:
+    try:
+        suffix = Path(upload_file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(upload_file.file, tmp)
+            tmp_path = Path(tmp.name)
+    finally:
+        upload_file.file.close()
+    return tmp_path
+
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile):
     processor = GridsetProcessor()
 
-    demo_dir = "./examples/demofiles"
-    gridset_file = os.path.join(demo_dir, "SimpleTest.gridset")
-
+    gridset_file = save_upload_file_tmp(file)
     texts = processor.extract_texts(gridset_file)
 
     translations = {}
     for i, text in enumerate(texts):
         translations[text] = f"Translated_{i}"
     translations["target_lang"] = "es"
-    print(f"Created translations: {translations}")
 
-    outputPath = os.path.join(demo_dir, "output.gridset")
+    translated = processor.create_translated_file(gridset_file, translations)
 
-    translated_file = processor.process_texts(
-        gridset_file,
-        translations,
-        outputPath
-    )
-
-    return {
-        "filename": file.filename,
-        "textCount": len(texts),
-        "outputPath": outputPath
-    }
+    return FileResponse(translated, media_type="application/octet-stream")
